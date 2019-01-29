7371a4fe12ad8e4f3667a0e2f5a5a23e44f66d76
MatthiasPL <smateusz96@gmail.com>
2019-01-29 13:07:43 +0100

    Added basic MainMenu layout

--- app/src/main/java/com/loopmoth/looplace/MainMenu.kt
+++ app/src/main/java/com/loopmoth/looplace/MainMenu.kt
@@ -2,11 +2,22 @@ package com.loopmoth.looplace
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bAdd.setOnClickListener {
+
+        }
+        bPlay.setOnClickListener {
+            
+        }
     }
 }
